c63a9ace682644d92619b1cf06ddeb4138c2ab89
Sai Sathwik Reddy <150815533+saisathwik22@users.noreply.github.com>
2025-12-30 14:58:01 +0530

    Implement two permutations problem
    Add a C++ implementation for generating two permutations with specified longest common prefix and suffix lengths.

---
+++ 800_rating/two-permutations.cpp
@@ -0,0 +1,19 @@
+// question
+// integers N, A, B given
+// a permutation is nums [1 to N], with unique nums
+
+// can you create two permutations p and q of size N ?
+// such that their longest common prefix is of length A
+// their longest common suffix is of length B
+// print yes or no
+
+// N = 4, A = 1, B = 1
+// p = [1, 2, 3, 4]
+// q = [1, 3, 2, 4]
+// longest common prefix is [1] of length 1
+// longest common suffix is [4] of length 1
+// answer - yes
+
+// Argument
+// if (a+b+2 <= n || a==b==n) "YES"
+// else "NO"
